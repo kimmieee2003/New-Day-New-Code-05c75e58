166e6edc7d212de9a146260dd9a13077284d46b4
Kimberly Hogeveen <2103405@talnet.nl>
2020-03-25 13:42:52 +0100

    VERBETER

--- wisselgeld.php
+++ wisselgeld.php
@@ -2,19 +2,20 @@
 $input = $argv[1];
 $bedrag = intval($input);
 
-define("geldeenheden", [
+define("GELDEENHEDEN",[
     50, 
     20,
     10, 
     5, 
     2, 
     1
-]);
+    ]
+);
 
 $restbedrag = $bedrag;
 
-foreach(geldeenheden as $geldeenheid){
-    if($restbedrag >= $geldeenheid){
+foreach(GELDEENHEDEN as $geldeenheid){
+    if($restbedrag >= $geldeenheid) {
         $aantalKeerGeldEenheidInRestBedrag = floor($restbedrag / $geldeenheid);
         $restbedrag = $restbedrag % $geldeenheid;
         print($aantalKeerGeldEenheidInRestBedrag. "*" .$geldeenheid. "euro".PHP_EOL);
