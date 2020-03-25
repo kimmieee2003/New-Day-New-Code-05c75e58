3e472f14738763a1a02bd93b8d784b3ea7905f01
Kimberly Hogeveen <2103405@talnet.nl>
2020-03-25 13:13:50 +0100

    Uitwerking van de exercise

---
+++ wisselgeld.php
@@ -0,0 +1,24 @@
+<?php
+$input = $argv[1];
+$bedrag = intval($input);
+
+define("geldeenheden", [
+    50, 
+    20,
+    10, 
+    5, 
+    2, 
+    1
+]);
+
+$restbedrag = $bedrag;
+
+foreach(geldeenheden as $geldeenheid){
+    if($restbedrag >= $geldeenheid){
+        $aantalKeerGeldEenheidInRestBedrag = floor($restbedrag / $geldeenheid);
+        $restbedrag = $restbedrag % $geldeenheid;
+        print($aantalKeerGeldEenheidInRestBedrag. "*" .$geldeenheid. "euro".PHP_EOL);
+    }
+}
+
+?>
